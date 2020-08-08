bc2725e09ee63da458aac83c51af4e80f332272f
Nikhil Keshav <nikhil.keshav@niser.ac.in>
2020-08-08 14:45:29 +0530

    Sum of first n positive integers

---
+++ Assignment 1/Q1.c
@@ -0,0 +1,20 @@
+// add 1+2+3+ ... +n WITHOUT using the formula n(n+1)/2
+
+//include required librarires
+#include <stdio.h>
+
+int main(){
+  //declare the variables
+  int sum, n;
+  sum = 0;
+  //take input from user
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  //intitiate a for loop
+  for (int i = 1; i < n + 1; i++){
+    sum = sum + i;
+  }
+  //print the output
+  printf("The sum of all positive integers till  %d", sum);
+  return 0;
+}
