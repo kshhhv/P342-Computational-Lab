be1f6e3b5b8dd89c126b48b2c91a0f1790222275
Nikhil Keshav <nikhil.keshav@niser.ac.in>
2020-08-14 20:42:53 +0530

    output statement updated

--- Assignment 2/Q2.py
+++ Assignment 2/Q2.py
@@ -1,19 +1,23 @@
-A = [1,2,3]
-B = [2,4,6]
+vec_A = [1,2,3]
+vec_B = [2,4,6]
 
-dim = len(A)
+print("First vector is ", vec_A)
+print("Second vector is ", vec_B)
+
+
+dim = len(vec_A)
 
 # Vector addition
 add = []
 for i in range(dim):
-	add.append(A[i] + B[i])
+	add.append(vec_A[i] + vec_B[i])
 
-print("The new vector is ", add)
+print("The sum vector is ", add)
 
 # Inner product of vectors
 in_pro = 0
 for i in range(dim):
-	pro = A[i]*B[i]
+	pro = vec_A[i]*vec_B[i]
 	in_pro += pro
 
 print("The dot product is", in_pro)
