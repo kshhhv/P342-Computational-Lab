ef8fc99b0d6ead8723225ac594aa7e5b865e86f2
Nikhil Keshav <nikhil.keshav@niser.ac.in>
2020-08-14 20:42:26 +0530

    distance don't commute

--- Assignment 2/Q1b.py
+++ Assignment 2/Q1b.py
@@ -1,4 +1,4 @@
-size = int(input("Enter the size of grid"))
+size = int(input("Enter the size of grid: "))
 points = []
 
 for m in range(size):
@@ -8,22 +8,18 @@ for m in range(size):
 	m += 1
 
 sum = 0
-pairs = 0
-print("List is", points)
+pairs = size**4
+
 tot_points = len(points)
 
 for i in range(tot_points):
-	print("From the point ", points[i])
 	x1,y1 = points[i]
-	for j in range(i+1, tot_points):
+	for j in range(tot_points):
 		x2,y2 = points[j]
-		distance = abs(x2 - x1) + abs(y2 - y1)
-		print("Distance: ", distance)
-		sum += distance
-		pairs += 1
+		sum += abs(x2 - x1) + abs(y2 - y1)
 
-print("Sum is", sum)
-print("Pair is", pairs)
+print("Sum of distance is",sum)
+print("Total number of pairs is",pairs)
 
 avg_dist = sum/pairs
-print("The average distance between two points is ", avg_dist)
+print("The average distance between two points is",avg_dist)
