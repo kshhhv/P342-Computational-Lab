37f0c0ce329416b67a064f13046fd39fff724418
Nikhil Keshav <nikhil.keshav@niser.ac.in>
2020-08-17 14:36:25 +0530

    Removed extra spaces

---
+++ Assignment 2/Q1a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	int size;
+	printf("Enter the number of linear equidistant points:");
+	scanf("%d", &size);
+
+	float tot_dist = 0;
+	int tot_pairs = size*size;
+
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < size; j++){
+			tot_dist += abs(j - i);
+		}
+	}
+
+	float avg_dist = tot_dist/tot_pairs;
+
+	printf("The average distance between two points is %.4f", avg_dist);
+
+	return 0;
+}
