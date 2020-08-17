2e48f72544d91de7d58dc1921a2e702ddd44b716
Nikhil Keshav <nikhil.keshav@niser.ac.in>
2020-08-17 14:36:58 +0530

    Removed extra print statements

--- Assignment 2/Q1a.py
+++ Assignment 2/Q1a.py
@@ -8,12 +8,8 @@ for n in range(size):
 tot_dist = 0
 tot_pairs = size*size
 
-print("List is", points)
-
 for i in points:
-	print("i is", i)
 	for j in points:
-		print("j is", j)
 		tot_dist += abs(j - i)
 
 avg_dist = tot_dist/tot_pairs
